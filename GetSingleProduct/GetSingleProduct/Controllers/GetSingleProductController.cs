3f2988fef8b8fb5f3dc995b546148b1487279430
joaodahub2b <97136063+joaodahub2b@users.noreply.github.com>
2022-03-18 21:35:30 -0300

    Add files via upload

---
+++ GetSingleProduct/GetSingleProduct/Controllers/GetSingleProductController.cs
@@ -0,0 +1,26 @@
+﻿using GetSingleProduct.Data.Dtos;
+using GetSingleProduct.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GetSingleProduct.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetSingleProductController : ControllerBase
+    {
+        MagentoService magentoService;
+
+        public GetSingleProductController(MagentoService magentoService)
+        {
+            this.magentoService = magentoService;
+        }
+
+        [HttpGet("{system}")]
+        public IActionResult GetProduct(string system, [FromBody] ReadMagentoDto magentoDto )
+        {
+            magentoService.GetSingleProduct(system, magentoDto);
+            return Ok();
+        }
+    }
+}
